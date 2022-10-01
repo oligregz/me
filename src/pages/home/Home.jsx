c75a8be88390ab44864260732919aa119ca9c0e4
gregory <gregorysak90@gmail.com>
2022-10-01 09:04:48 -0300

    refect: mudando texto da home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="case">
       <div className="meHome">
         <p className="txt" id="oi">Hey ___________</p>
-        <p className="txt" id="eu">EU SOU O GREGORY OLIVEIRA</p>
+        <p className="txt" id="eu">EU SOU O GREGZ</p>
         <p className="txt" id="dev">DESENVOLVEDOR <br></br> FULLSTACK</p>
       </div> 
       <Footer />
